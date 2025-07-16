32d555c969d653d851309f418004275c98c69035
joy267 <joymandal267@gmail.com>
2025-07-16 20:12:02 +0530

    new

--- cypress/e2e/aladdin_theme.cy.js
+++ cypress/e2e/aladdin_theme.cy.js
@@ -24,23 +24,23 @@ describe('Create Theme', () => {
   })
   })
 
-//describe('Edit Theme', () => {
-//
-//  it('Edit Theme Successfully', () => {
-//    cy.visit('https://aladdin-ui-stage.etloptival.com/')
-//    cy.get('input[placeholder="Enter Your Email"]').type('tester@etloptival.com')
-//    cy.get('input[id=":r1:"]').type('soliva@123')
-//    cy.contains('button', 'LOGIN NOW').click()
-//    cy.get('div[aria-label="Assets"]').click()
-//    cy.contains('span', 'Design').click()
-//    cy.contains('span', 'Themes').click()
-//    cy.get('input[placeholder="Search site name or choose from the list"]').click()
-//    cy.get('input[placeholder="Search site name or choose from the list"]').type('{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}').type('{enter}')
-//    cy.contains('button', 'SELECT SITE').click()
-//    cy.get('svg[data-testid="CreateOutlinedIcon"]').first().click()
-//    cy.contains('button', 'Save Changes').click()
-//    cy.contains('button', 'Close').click()
-//
-//
-//  })
-//  })
+describe('Edit Theme', () => {
+
+  it('Edit Theme Successfully', () => {
+    cy.visit('https://aladdin-ui-stage.etloptival.com/')
+    cy.get('input[placeholder="Enter Your Email"]').type('tester@etloptival.com')
+    cy.get('input[id=":r1:"]').type('soliva@123')
+    cy.contains('button', 'LOGIN NOW').click()
+    cy.get('div[aria-label="Assets"]').click()
+    cy.contains('span', 'Design').click()
+    cy.contains('span', 'Themes').click()
+    cy.get('input[placeholder="Search site name or choose from the list"]').click()
+    cy.get('input[placeholder="Search site name or choose from the list"]').type('{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}').type('{enter}')
+    cy.contains('button', 'SELECT SITE').click()
+    cy.get('svg[data-testid="CreateOutlinedIcon"]').first().click()
+    cy.contains('button', 'Save Changes').click()
+    cy.contains('button', 'Close').click()
+
+
+  })
+  })
